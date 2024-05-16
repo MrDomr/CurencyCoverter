59f98fb58612e1dc7c1b0519742406b8f88b0dcb
domr <dominykas.repovas@gmail.com>
2024-05-16 17:22:24 +0300

    Removed files that are not needed

--- src/Main.java
+++ src/Main.java
@@ -8,51 +8,11 @@ import java.util.HashMap;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 public class Main {
-    private static final String API_KEY = "fca_live_m55KwmNC2VYX0dcfWM04hXGanbDzUe7mM9xyUd7b";
-    private static final String BASE_URL = "https://api.freecurrencyapi.com/v1/latest";
+
 
     public static void main(String[] args) throws Exception {
         // Create a Url object from the BASE_URL and API_KEY
-        URL url = new URL(BASE_URL + "?apikey=" + API_KEY);
-
-        // Create a connection
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        // Get the response code
-        int responseCode = connection.getResponseCode();
-
-        // If the request was successful
-        if(responseCode == 200){
-            // Create a BufferedReader to read the response
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            StringBuffer responseContent = new StringBuffer();
-
-            // Read the response
-            while((line = reader.readLine()) != null){
-                responseContent.append(line);
-                System.out.println(line);
-            }
-
-            // Close the reader and connection
-            reader.close();
-            connection.disconnect();
-
-            // Parse the JSON response
-            String jsonData = responseContent.toString();
-            // Create a Gson object
-            Gson gson = new Gson();
-
-            // Define the type for the HashMap
-            Type type = new TypeToken<HashMap<String, Double>>(){}.getType();
-
-            // Parse the JSON data to a HashMap
-            HashMap<String, Double> data = gson.fromJson(gson.fromJson(jsonData, JsonObject.class).getAsJsonObject("data"), type);
-
-            // Print the HashMap
-            for(String key : data.keySet()) {
-                System.out.println(key + ": " + data.get(key));
-            }
-        }
+        DataRead reader = new DataRead();
+        HashMap<String, Double> curren
     }
 }
